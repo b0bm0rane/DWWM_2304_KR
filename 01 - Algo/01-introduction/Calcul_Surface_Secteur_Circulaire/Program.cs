a20a905087a2a366d1e46637bed8e787015d7ecc
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-04-26 09:18:47 +0200

    fin 1.x

---
+++ 01 - Algo/01-introduction/Calcul_Surface_Secteur_Circulaire/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exo_1_5_Calcul_Surface_Secteur_Circulaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double angle;
+            double rayon;
+            double surface;
+
+            Console.WriteLine("Bienvenue dans le programme de calcul de la surface d'un secteur circulaire");
+
+            Console.WriteLine("Veuillez saisir l'angle en degrés");
+
+            angle = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir le rayon en cm");
+
+            rayon = double.Parse(Console.ReadLine());
+
+            surface = (Math.PI * Math.Pow(rayon,2) * angle) / 360;
+
+            Console.WriteLine("La surface du secteur circulaire est de " + surface + " cm2.");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 01 - Algo/01-introduction/Exo_1_6_Inversion_2_Valeurs/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Exo_1_6_Inversion_2_Valeurs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double temp;
+
+            Console.WriteLine("Bienvenue dans le programme d'inversion de 2 valeurs");
+
+            Console.WriteLine("Veuillez saisir le nombre a");
+
+            a = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Veuillez saisir le nombre b");
+
+            b = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Le nombre a = " + a + ". Le nombre b = " + b, ".");
+
+            Console.ReadLine();
+
+            temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine("Le nombre a = " + a + ". Le nombre b = " + b, ".");
+        }
+    }
+}
