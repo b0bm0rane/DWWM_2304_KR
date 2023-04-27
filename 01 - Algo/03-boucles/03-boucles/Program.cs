c88ae19af958fca44b9ae969ade98a6e80f75530
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-04-27 08:51:09 +0200

    controle saisie

--- 01 - Algo/03-boucles/03-boucles/Program.cs
+++ 01 - Algo/03-boucles/03-boucles/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace _03_boucles
+﻿namespace Exo_3_1_Controle_Saisie
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenue dans le programme");
+            string prenom;
+
+            Console.WriteLine("Bienvenue dans le programme de controle de saisie");
+
+            do
+            {
+
+                Console.WriteLine("Veuillez saisir votre prénom (2 caractères minimum)");
+
+                prenom = Console.ReadLine();
+
+            }
+
+            while (prenom.Length < 2);
+
+            Console.WriteLine("Bonjour " + prenom);
+
+            Console.ReadLine();
+
         }
     }
 }
