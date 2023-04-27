9420d8c3c886976e66d64f8cb690fcf69495a3e9
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-04-27 16:02:07 +0200

    intervalle 2 nombres

---
+++ 01 - Algo/03-boucles/Exo_3_1_Controle_Saisie_Limite/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Reflection;
+
+namespace Exo_3_2_Controle_Saisie_Limite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string saisie;
+            string mdp;
+            int essais;
+            Boolean estConnecte;
+
+            mdp = "formation";
+            essais = 1;
+            estConnecte = false;
+
+            Console.WriteLine("Bienvenue dans le programme de saisie limite");
+
+            while (essais <= 3 && !estConnecte)
+            {
+
+                Console.WriteLine("Saisissez votre mot de passe");
+
+                saisie = Console.ReadLine();
+
+                if (saisie == mdp)
+                {
+
+                    estConnecte = true;
+
+                }
+
+                else
+                {
+
+                    essais = essais + 1;
+
+                }
+
+            }
+
+            if (estConnecte)
+            {
+
+                Console.WriteLine("Vous êtes connecté");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Votre compte est bloqué");
+
+            }
+        }
+    }
+}
---
+++ 01 - Algo/03-boucles/Exo_3_3_Intervalle_2_Nombres/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Exo_3_3_Intervalle_2_Nombres
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int i;
+
+            Console.WriteLine("Bienvenue dans le programme d'intervalle entre 2 nombres");
+
+            Console.WriteLine("Saisissez le premier nombre");
+
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Saisissez le second nombre");
+
+            b = int.Parse(Console.ReadLine());
+
+            for (i = a + 1; i < b; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+    }
+}
