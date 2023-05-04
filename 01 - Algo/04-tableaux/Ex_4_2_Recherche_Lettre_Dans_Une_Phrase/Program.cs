010b89e9d2adc369d2a71d99ebe161af53eb8c11
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-05-04 11:35:47 +0200

    fin 4.1 et 4.2

--- 01 - Algo/04-tableaux/Ex_4_2_Recherche_Lettre_Dans_Une_Phrase/Program.cs
+++ 01 - Algo/04-tableaux/Ex_4_2_Recherche_Lettre_Dans_Une_Phrase/Program.cs
@@ -6,16 +6,61 @@ namespace Ex_4_2_Recherche_Lettre_Dans_Une_Phrase
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Bienvenue dans le programme de recherche d'une lettre dans une phrase");
-
-            string lettre;
+            char lettre;
             string phrase;
+            int indice;
+            int occurences;
+
+            Console.WriteLine("Bienvenue dans le programme de recherche d'une lettre dans une phrase");
 
             phrase = "J'ai un chat.";
 
             Console.WriteLine("Saissez une lettre");
 
-            lettre = Console.ReadLine();
+            lettre = char.Parse(Console.ReadLine());
+
+            indice = 0;
+            occurences = 0;
+
+            if (phrase == "" || phrase == ".")
+            {
+
+                Console.WriteLine("La chaine est vide");
+
+            }
+
+            else
+            {
+
+                for (indice = 0 ; indice < phrase.Length ; indice++)
+                {
+
+                    if (phrase[indice] == lettre)
+                    {
+
+                        occurences++;
+                    }
+
+                }
+
+                if (occurences > 0)
+                {
+
+                    Console.WriteLine("La lettre " + lettre + " est présente " + occurences + " fois dans la phrase");
+
+                }
+
+                else
+                {
+
+                    Console.WriteLine("La lettre " + lettre + " n'est pas présente");
+
+                }
+
+            }
+
+        }
+
             /*
             if ( phrase == "" || phrase == ".")
             {
@@ -46,6 +91,5 @@ namespace Ex_4_2_Recherche_Lettre_Dans_Une_Phrase
 
 
 
-        }
     }
 }
--- 01 - Algo/04-tableaux/Exo_4_1_Recherche_Nombre_Tableau/Program.cs
+++ 01 - Algo/04-tableaux/Exo_4_1_Recherche_Nombre_Tableau/Program.cs
@@ -4,6 +4,55 @@
     {
         static void Main(string[] args)
         {
+            int[] tableau = { 8, 16, 32, 64, 128, 256, 512 };
+            int indice;
+            int nombreATrouver; // nombre à trouver
+            Boolean aTrouve;
+
+            Console.WriteLine("Bienvenue dans le programme de recherche d'un nombre dans un tableau");
+
+            Console.WriteLine("Saississez un nombre");
+
+            nombreATrouver = int.Parse(Console.ReadLine());
+
+            indice = 0;
+            aTrouve = false;
+
+            while (!aTrouve && ( indice < tableau.Length ))
+            {
+
+                if (tableau[indice] == nombreATrouver)
+                {
+
+                    aTrouve = true;
+
+                }
+
+                else
+                {
+
+                    indice++;
+
+                }
+
+            }
+
+            if (aTrouve)
+            {
+
+                Console.WriteLine("L'indice est " + indice);
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Non trouvé");
+
+            }
+
+            /*
+            
             int a;
             int[] tab = { 8, 16, 32, 64, 128, 256, 512 };
 
@@ -26,6 +75,8 @@
                 Console.WriteLine("Nombre non trouvé");
 
             }
+            
+            */
 
         }
     }
