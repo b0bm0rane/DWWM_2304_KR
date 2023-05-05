fe60f61fcf478707f304fff57ba80abdb384aaf2
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-05-05 11:49:31 +0200

    tri tableau

---
+++ 01 - Algo/04-tableaux/Exo_4_4_Tri_Tableau/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Exo_4_4_Tri_Tableau
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] tableau = { 128, 64, 8, 512, 16, 32, 256 };
+            int indiceA;
+            int indiceB;
+            int temp;
+
+            Console.WriteLine("Bienvenue dans le programme de tri d'un tableau");
+
+            Console.WriteLine(tableau[0]);
+        }
+    }
+}
