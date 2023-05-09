8ec1e2af982f7dfc7e3eb732288b3bc465a48fb4
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-05-09 15:48:07 +0200

    tri tableau et palindrome

--- 01 - Algo/04-tableaux/Exo_4_4_Tri_Tableau/Program.cs
+++ 01 - Algo/04-tableaux/Exo_4_4_Tri_Tableau/Program.cs
@@ -5,13 +5,39 @@
         static void Main(string[] args)
         {
             int[] tableau = { 128, 64, 8, 512, 16, 32, 256 };
-            int indiceA;
-            int indiceB;
             int temp;
+            int indiceMin;
 
             Console.WriteLine("Bienvenue dans le programme de tri d'un tableau");
 
-            Console.WriteLine(tableau[0]);
+            Console.WriteLine(tableau[0] + ", " + tableau[1] + ", " + tableau[2] + ", " + tableau[3] + ", " + tableau[4] + ", " + tableau[5] + ", " + tableau[6]);
+
+
+            for (int indiceA = 0; indiceA < tableau.Length; indiceA++)
+            {
+
+                indiceMin = indiceA;
+
+                for (int indiceB = indiceA + 1; indiceB < tableau.Length; indiceB++)
+                {
+                    if (tableau[indiceB] < tableau[indiceMin])
+                    {
+
+                        indiceMin = indiceB;
+
+                    }
+
+                   
+                }
+
+                temp = tableau[indiceMin];
+                tableau[indiceMin] = tableau[indiceA];
+                tableau[indiceA] = temp;
+
+            }
+
+            Console.WriteLine(tableau[0] + ", " + tableau[1] + ", " + tableau[2] + ", " + tableau[3] + ", " + tableau[4] + ", " + tableau[5] + ", " + tableau[6]);
+
         }
     }
 }
---
+++ 01 - Algo/04-tableaux/Exo_4_5_Palindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Exo_4_5_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string texte;
+            int indiceA;
+
+            Console.WriteLine("Bienvenue dans le programme du palindrome");
+
+            Console.WriteLine("Saisissez du texte");
+
+            texte = Console.ReadLine();
+
+            indiceA = 0;
+
+            while (indiceA < (texte.Length - 1 - indiceA))
+            {
+
+                indiceA++;
+
+            }
+
+            if (indiceA > (texte.Length - 1 - indiceA))
+            {
+
+                Console.WriteLine("Le texte est un palindrome");
+
+            }
+
+            else
+            {
+
+                Console.WriteLine("Le texte n'est pas un palindrome");
+
+            }
+
+        }
+    }
+}
