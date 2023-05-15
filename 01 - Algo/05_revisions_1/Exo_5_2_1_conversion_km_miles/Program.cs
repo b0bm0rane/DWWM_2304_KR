6d5d2dafb2311fd1e13d606bdeb6c0da947db625
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-05-15 15:54:44 +0200

    conversion km miles

---
+++ 01 - Algo/05_revisions_1/Exo_5_2_1_conversion_km_miles/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Exo_5_2_1_conversion_km_miles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valeurKm;
+            double valeurMiles;
+            string sortie;
+
+            Console.WriteLine("Bienvenue dans le programme de conversion kilomètres <--> miles");
+
+            sortie = "q";
+
+            do
+            {
+
+                Console.WriteLine("Saisissez une valeur en km comprise entre 0,01 et 1 000 000 ou q pour quitter le programme");
+
+                valeurKm = double.Parse(Console.ReadLine());
+                
+            }
+
+            while (valeurKm < 0.01 || valeurKm > 1000000);
+
+            valeurMiles = valeurKm * 1.609;
+
+            Console.WriteLine(valeurKm + " km = " + valeurMiles + " miles");
+
+            if (valeurKm = sortie)
+            {
+
+                Console.WriteLine("Fin du programme");
+                
+            }
+
+        }
+    }
+}
