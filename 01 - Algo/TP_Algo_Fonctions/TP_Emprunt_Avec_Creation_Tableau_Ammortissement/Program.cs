d817bba6b46bc67ee6a4dcf41ba0c1443b778284
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-05-26 11:35:54 +0200

    Ajout tp emprunt

---
+++ 01 - Algo/TP_Algo_Fonctions/TP_Emprunt_Avec_Creation_Tableau_Ammortissement/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TP_Emprunt_Avec_Creation_Tableau_Ammortissement
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
