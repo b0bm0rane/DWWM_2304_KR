ee00e093c7eb60df25837cac5da7a0e7ee77cce7
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-08 13:34:03 +0200

    maj bouteille vs

---
+++ Objet/Bouteille/Bouteille/CL_Bouteille/Bouteille.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Bouteille
+{
+    public class Bouteille
+    {
+        private bool alcool;
+        private float contenanceEnL;
+        private float contenuEnL;
+        private bool estGazeux;
+        private string matiere;
+        private string nomBoisson;
+        private bool estOuverte;
+
+        public Bouteille(bool _alcool, float _contenanceEnL, float _contenuEnL, bool _estGazeux, string _matiere, string _nomBoisson, bool _estOuverte)
+        {
+
+            this.alcool = _alcool;
+            this.contenanceEnL = _contenanceEnL;
+            this.contenuEnL = _contenuEnL;
+            this.estGazeux = _estGazeux;
+            this.matiere = _matiere;
+            this.nomBoisson = _nomBoisson;
+            this.estOuverte = _estOuverte;
+
+        }
+
+        public bool Ouvrir()
+        {
+
+            if (!estOuverte)
+            {
+
+                this.estOuverte = true;
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+        public bool Fermer()
+        {
+
+            if (estOuverte)
+            {
+                this.estOuverte = false;
+                
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+    }
+}
---
+++ Objet/Bouteille/Bouteille/Projet_Bouteille/Program.cs
@@ -0,0 +1,17 @@
+﻿using CL_Bouteille;
+using System.Runtime.CompilerServices;
+
+namespace Projet_Bouteille
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bouteille bouteilleDeCoke = new Bouteille(false, 0.5f, 0, true, "plastique", "Coke", false);
+
+            bouteilleDeCoke.Ouvrir();
+
+        }
+    }
+}
