5d83e21060f642d2f339a9fcce23e04bddb8703c
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-19 12:47:50 +0200

    ajout lepidoptere vs

---
+++ Objet/Lepidoptere/Solution_Lepidoptere/Console_Lepidoptere/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Console_Lepidoptere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
