1e5f950bf4635e0fdc32a551dec17cd10c3d59cf
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-20 10:53:40 +0200

    maj lepidoptere vs

--- Objet/Lepidoptere/Solution_Lepidoptere/CL_Lepidoptere/Lepidoptere.cs
+++ Objet/Lepidoptere/Solution_Lepidoptere/CL_Lepidoptere/Lepidoptere.cs
@@ -8,5 +8,35 @@ namespace CL_Lepidoptere
 {
     internal class Lepidoptere
     {
+
+        private bool estMouvant;
+        private bool estVivant;
+
+        public Lepidoptere()
+        {
+
+            estMouvant = true;
+            estVivant = true;
+
+        }
+
+        public bool SeDeplacer()
+        {
+
+            if (this.estMouvant && this.estVivant)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
     }
 }
