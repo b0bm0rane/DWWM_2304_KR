50c0e083e82fe96ed36d1d8583944da99ef0d43d
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-21 15:45:45 +0200

    voiture vs

--- Objet/Voiture/Solution_Voiture/CL_Voiture/Moteur.cs
+++ Objet/Voiture/Solution_Voiture/CL_Voiture/Moteur.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,65 @@ namespace CL_Voiture
 {
     internal class Moteur
     {
+
+        private bool estEnMarche;
+
+        public Moteur()
+        {
+
+            estEnMarche = true;
+
+        }
+
+        public bool Demarrer()
+        {
+
+            if (!this.estEnMarche)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+        public bool Eteindre()
+        {
+
+            if (this.estEnMarche)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+        public bool ArreterRoues()
+        {
+
+
+
+        }
+
+        public bool EntrainerRoues()
+        {
+
+
+
+        }
+
     }
 }
--- Objet/Voiture/Solution_Voiture/CL_Voiture/Roue.cs
+++ Objet/Voiture/Solution_Voiture/CL_Voiture/Roue.cs
@@ -8,5 +8,53 @@ namespace CL_Voiture
 {
     internal class Roue
     {
+
+        private int dimensionPneuEnCm;
+        private double vitesseEnKmh;
+
+        public Roue()
+        {
+
+            this.dimensionPneuEnCm = 50;
+            this.vitesseEnKmh = 10;
+
+        }
+
+        public bool Stopper()
+        {
+
+            if (this.vitesseEnKmh > 0)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
+        public bool Tourner()
+        {
+
+            if (this.vitesseEnKmh > 0)
+            {
+
+                return true;
+
+            }
+            else
+            {
+
+                return false;
+
+            }
+
+        }
+
     }
 }
