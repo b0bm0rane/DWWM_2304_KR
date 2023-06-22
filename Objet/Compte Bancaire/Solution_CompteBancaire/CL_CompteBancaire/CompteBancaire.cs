4ce86318f50a2e47fae27aa92264b7f825ca2ce1
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-22 14:44:09 +0200

    ajout compte bancaire ea et vs

---
+++ Objet/Compte Bancaire/Solution_CompteBancaire/CL_CompteBancaire/CompteBancaire.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_CompteBancaire
+{
+    public class CompteBancaire
+    {
+
+        private int decouvertAutorise;
+        private string nom;
+        private int numero;
+        private double solde;
+
+        public CompteBancaire(int _decouvertAutorise, string _nom, int _numero, double _solde)
+        {
+
+            this.decouvertAutorise = _decouvertAutorise;
+            this.nom = _nom;
+            this.numero = _numero;
+            this.solde = _solde;
+
+        }
+
+
+
+    }
+}
---
+++ Objet/Compte Bancaire/Solution_CompteBancaire/Console_CompteBancaire/Program.cs
@@ -0,0 +1,16 @@
+﻿using CL_CompteBancaire;
+
+namespace Console_CompteBancaire
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            CompteBancaire CBFlorian = new CompteBancaire(-500, "Florian", 1234, 2000);
+
+            Console.WriteLine(CBFlorian.ToString());
+
+        }
+    }
+}
