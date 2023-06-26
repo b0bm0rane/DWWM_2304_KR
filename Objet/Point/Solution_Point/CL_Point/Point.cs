31b0e55caf59441b50fcb3d6e2a19ca606cf541a
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-26 15:59:40 +0200

    maj point vs

--- Objet/Point/Solution_Point/CL_Point/Point.cs
+++ Objet/Point/Solution_Point/CL_Point/Point.cs
@@ -1,12 +1,80 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace CL_Point
 {
-    internal class Point
+    public class Point
     {
+
+        private decimal x;
+        private decimal y;
+
+        public Point()
+        {
+
+            this.x = 0;
+            this.y = 0;
+
+        }
+
+        public Point(decimal _x, decimal _y)
+        {
+
+            this.x = _x;
+            this.y = _y;
+
+        }
+
+        public override string ToString()
+        {
+
+            return "Abscisse : " + this.x + "|" + "Ordonnée : " + this.y;
+        
+        }
+
+        public void Deplacer(decimal nouveauX, decimal nouveauY)
+        {
+
+            this.x = nouveauX;
+            this.y = nouveauY;
+
+        }
+
+        public Point SymOrdonnees()
+        {
+
+            Point pointSymOrdonnees = new Point(-this.x, this.y);
+            
+            return pointSymOrdonnees;
+
+        }
+        /*
+        public Point SymAbscisses()
+        {
+
+
+
+        }
+
+        public Point SymOrigine()
+        {
+
+            
+
+        }
+        */
+        public void Permuter()
+        {
+
+            decimal temp = this.x;
+            this.x = this.y;
+            this.y = temp;
+
+        }
+
     }
 }
--- Objet/Point/Solution_Point/Console_Point/Program.cs
+++ Objet/Point/Solution_Point/Console_Point/Program.cs
@@ -1,10 +1,30 @@
-﻿namespace Console_Point
+﻿using CL_Point;
+
+namespace Console_Point
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Point point0 = new Point();
+
+            Point point1 = new Point(1, 5);
+
+            Console.WriteLine(point0.ToString());
+
+            Console.WriteLine(point1.ToString());
+
+            point0.Deplacer(5, 10);
+
+            Console.WriteLine(point0.ToString());
+
+            point0.Permuter();
+
+            Console.WriteLine(point0.ToString());
+
+            point0.SymOrdonnees();
+
         }
     }
 }
