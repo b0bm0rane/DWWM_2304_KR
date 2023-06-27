8083e40f6e94ffae904177ce400fa815e1d48478
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-27 16:03:59 +0200

    ajout fraction ea vs

---
+++ Objet/Fraction/Solution_Fraction/CL_Fraction/Fraction.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Fraction
+{
+    public class Fraction
+    {
+        private int numerateur;
+        private int denominateur;
+
+        public Fraction()
+        {
+            numerateur = 0;
+            denominateur = 1;
+        }
+
+        public Fraction(int _numerateur, int _denominateur)
+        {
+            this.numerateur = _numerateur;
+            this.denominateur = _denominateur;
+        }
+
+        public Fraction(int _numerateur)
+        {
+            this.numerateur = _numerateur;
+            this.denominateur = 1;
+        }
+
+        public override string ToString()
+        {
+            return this.numerateur + "/" + this.denominateur;
+        }
+
+        public double Resultat()
+        {
+            return (double) this.numerateur / this.denominateur;
+        }
+
+        public void Oppose()
+        {
+            this.numerateur = -this.numerateur;
+        }
+
+        public void Inverse()
+        {
+            int temp = this.numerateur;
+            this.numerateur = this.denominateur;
+            this.denominateur = temp;
+        }
+
+        public bool SuperieurA(Fraction fractionAComparer)
+        {
+            return this.Resultat() > fractionAComparer.Resultat();
+        }
+
+        public bool EgalA(Fraction fractionAComparer)
+        {
+            return this.Resultat() == fractionAComparer.Resultat();
+        }
+
+        private int GetPgdc()
+        {
+            int a = this.numerateur;
+            int b = this.denominateur;
+            int pgdc = 1;
+
+            if (a != 0 && b != 0)
+            {
+                    if (a < 0) a = -a;
+                    if (b < 0) b = -b;    
+                    while(a != b)
+                    {
+                        if (a < b)
+                        {
+                            b = b - a;
+                        }
+                        else
+                        {
+                            a = a - b;
+                        }
+                    }
+                    pgdc = a;
+            }
+            return pgdc;
+        }
+
+        private void Reduire()
+        {
+            
+        }
+
+    }
+}
---
+++ Objet/Fraction/Solution_Fraction/Console_Fraction/Program.cs
@@ -0,0 +1,32 @@
+﻿using CL_Fraction;
+
+namespace Console_Fraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Fraction fraction1 = new Fraction(12, 7);
+
+            Fraction fraction2 = new Fraction();
+
+            Fraction fraction3 = new Fraction(9);
+
+            // Console.WriteLine(fraction1.Resultat());
+
+            //Fraction oppose = fraction1.Oppose();
+
+            //Console.WriteLine(oppose.ToString());
+
+            Console.WriteLine(fraction1.ToString());
+
+            Console.WriteLine(fraction1.Resultat());
+
+            fraction1.Inverse();
+
+            Console.WriteLine(fraction1.ToString());
+
+            Console.WriteLine(fraction1.Resultat());
+        }
+    }
+}
