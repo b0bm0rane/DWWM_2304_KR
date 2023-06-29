45aa5615123480d9e1bf0d838615d9d57a793e45
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-06-29 12:18:55 +0200

    jalon ea vs

---
+++ Objet/Jalon_29_06_2023/Solution_Jalon_29_06_2023/CL_Guerre/Guerrier.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CL_Guerre
+{
+    public class Guerrier
+    {
+        private string nom;
+        private int vie;
+        private string nomArme1;
+        private string nomArme2;
+        private int puissanceArme1;
+        private int puissanceArme2;
+        private int puissanceTotale;
+
+        public Guerrier(string _nom, string _nomArme1, string _nomArme2, int _puissanceArme1, int _puissanceArme2)
+        {
+            this.nom = _nom;
+            this.vie = 10;
+            this.nomArme1 = _nomArme1;
+            this.nomArme2 = _nomArme2;
+            this.puissanceArme1 = _puissanceArme1;
+            this.puissanceArme2 = _puissanceArme2;
+            this.puissanceTotale = this.puissanceArme1 + this.puissanceArme2;
+        }
+
+        public int VieRestante()
+        {
+            return vie;
+        }
+
+        public bool Attaquer (Guerrier guerrierAttaqué)
+        {
+            if (this.puissanceTotale > guerrierAttaqué.puissanceTotale)
+            {
+                guerrierAttaqué.vie--;
+                return true;
+            }
+            else
+            {
+                this.vie--;
+                return false;
+            }
+        }
+    }
+}
---
+++ Objet/Jalon_29_06_2023/Solution_Jalon_29_06_2023/Console_Jalon_29_06_2023/Program.cs
@@ -0,0 +1,20 @@
+﻿using CL_Guerre;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Console_Jalon_29_06_2023
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guerrier guerrier1 = new Guerrier("guerrier1", "dague", "épée", 2, 4);
+            Guerrier guerrier2 = new Guerrier("guerrier2", "sabre", "masse", 5, 9);
+
+            guerrier1.Attaquer(guerrier2);
+
+            Console.WriteLine(guerrier1.VieRestante());
+            Console.WriteLine(guerrier2.VieRestante());
+        }
+    }
+}
