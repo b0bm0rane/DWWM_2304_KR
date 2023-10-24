a21ed8a7d3ec8ab4a62f9f0fd95ca61a86cef90d
b0bm0rane <120396384+b0bm0rane@users.noreply.github.com>
2023-10-24 16:10:19 +0200

    cp

---
+++ HTML/Codes_postaux/assets/js/main.js
@@ -0,0 +1,37 @@
+const apiUrl = "https://arfp.github.io/tp/web/html-css-js/03-zipcodes/zipcodes.json";
+// const { createApp } = Vue;
+
+// async function afficherCodePostal(){
+//     const reponse = await fetch(apiUrl);
+//     const codePostal = await reponse.json();
+//     console.log(codePostal);
+// }
+
+// createApp(afficherCodePostal).mount("#app");
+const list = document.getElementById("cpDataList")
+var cp;
+// var cpArray = [];
+
+fetch(apiUrl)
+.then((response)=> response.json())
+.then((json) => {
+cp = json;
+/*console.log(cp[0].nomCommune*/
+ajoutCP();
+// console.log(cpArray);
+});
+
+
+function ajoutCP(){
+    let html = '';
+    for(let i = 0; i < cp.length; i++){
+        html += `<option value="${cp[i].codePostal}">${cp[i].nomCommune}</option>`
+        // cpArray.push(cp[i].codePostal);
+        //ajoutOptionCP(cp[i].codePostal);
+       // console.log(cp[i].codePostal)
+    };
+    list.innerHTML = html;
+};
+
+
+console.log(list);
